6e48e79dbcbb9bbfa6b81d458f10742cd9c1c773
Jibendu Paul <bbiswas.paul@gmail.com>
2021-10-03 11:47:19 +0530

    added Color Picker with JS

---
+++ Color Picket with JS/script.js
@@ -0,0 +1,10 @@
+let colorInput = document.querySelector('#color');
+let hexInput = document.querySelector('#hex');
+
+colorInput.addEventListener('input', () =>{
+    let color = colorInput.value;
+    hexInput.value = color;
+    // document.body.style.backgroundColor = color;
+
+    document.querySelector('h1').style.color = color;
+});
